210a83d91eeb5894a3adf15a7121dda1ed0842c6
Thomas Tsilimigras <45098109+toboggann@users.noreply.github.com>
2024-10-24 09:59:16 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+
+
+        Wordle wordleTest = new Wordle();
+        wordleTest.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        wordleTest.setSize(900, 1080);
+        wordleTest.setVisible(true);
+    }
+
+
+
+}
